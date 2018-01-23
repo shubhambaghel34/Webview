71f9508529c3baebb9731d19e78193220e78cc72
Enthusiastic_coder <21123907+shubhambaghel34@users.noreply.github.com>
2018-01-23 12:20:41 +0530

    Webview in xamarin
    Use of Webview

---
+++ WebviewApp/WebviewPage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WebviewApp
+{
+  public  class WebviewPage: ContentPage
+
+    {
+        public WebviewPage()
+        {
+                        BackgroundColor = Color.MistyRose;
+            Label header = new Label
+            {
+                Text = "WebView",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center
+            };
+           
+            WebView webView = new WebView
+            {
+                Source = new UrlWebViewSource
+                {
+                    
+                    Url = "https://www.google.co.in",
+                },
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            WebView web2 = new WebView
+            {
+                Source = new HtmlWebViewSource
+                {
+                    Html = "<html><body>Hello</body></html>"
+                }
+            };
+            Padding = new Thickness(10, 20, 10, 5);
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    header,
+                    webView,
+                    web2
+                }
+            };
+        }
+
+
+
+
+    }
+}
